c316925d9dee24b5729dc808d7c511277896df4b
pctronique <pctronique@free.fr>
2024-05-14 20:55:06 +0200

    IS_MODIF_NAME

--- .docker/images/angular/modifname.sh
+++ .docker/images/angular/modifname.sh
@@ -11,9 +11,13 @@ then
 fi
 
 VAR_NAME_DEF=$(sed -ne "0,/\\\"name\": \".*\",/s//\0/p" ${NODE_FOLDER_PROJECT}/package.json)
-VAR_MODIF_NAME="\"name\": \"${NODE_NAME_PROJECT}\","
-
 VAR_NAME_DEL=$(echo "${VAR_NAME_DEF}" | sed -e 's/"name": //g' -e 's/"//g' -e 's/,//g')
+
+VAR_NAME_VAL=$( array=( $(echo "${NODE_NAME_PROJECT}" | grep -E -o "[a-zA-Z0-9]*") ); IFS=''; echo "${array[*]}" )
+VAR_NAME_LOW=$( echo "${VAR_NAME_VAL}" | tr '[:upper:]' '[:lower:]')
+#VAR_NAME_FRT_UP="${VAR_NAME_LOW^}"
+
+VAR_MODIF_NAME="\"name\": \"${VAR_NAME_LOW}\","
 
 if [ -e ${NODE_FOLDER_PROJECT}/package.json ]
 then
@@ -49,11 +53,11 @@ fi
 
 if [ -e ${NODE_FOLDER_PROJECT}/angular.json ]
 then
-  sed -i "s/\"${VAR_NAME_DEL}\":/\"${NODE_NAME_PROJECT}\":/i" "${NODE_FOLDER_PROJECT}/angular.json"
-  sed -i "s/\"outputPath\": \"dist\/${VAR_NAME_DEL}\"/\"outputPath\": \"dist\/${NODE_NAME_PROJECT}\"/i" "${NODE_FOLDER_PROJECT}/angular.json"
-  sed -i "s/\"buildTarget\": \"${VAR_NAME_DEL}:build:production\"/\"buildTarget\": \"${NODE_NAME_PROJECT}:build:production\"/i" "${NODE_FOLDER_PROJECT}/angular.json"
-  sed -i "s/\"buildTarget\": \"${VAR_NAME_DEL}:build:development\"/\"buildTarget\": \"${NODE_NAME_PROJECT}:build:development\"/i" "${NODE_FOLDER_PROJECT}/angular.json"
-  sed -i "s/\"buildTarget\": \"${VAR_NAME_DEL}:build\"/\"buildTarget\": \"${NODE_NAME_PROJECT}:build\"/i" "${NODE_FOLDER_PROJECT}/angular.json"
+  sed -i "s/\"${VAR_NAME_DEL}\":/\"${VAR_NAME_LOW}\":/i" "${NODE_FOLDER_PROJECT}/angular.json"
+  sed -i "s/\"outputPath\": \"dist\/${VAR_NAME_DEL}\"/\"outputPath\": \"dist\/${VAR_NAME_LOW}\"/i" "${NODE_FOLDER_PROJECT}/angular.json"
+  sed -i "s/\"buildTarget\": \"${VAR_NAME_DEL}:build:production\"/\"buildTarget\": \"${VAR_NAME_LOW}:build:production\"/i" "${NODE_FOLDER_PROJECT}/angular.json"
+  sed -i "s/\"buildTarget\": \"${VAR_NAME_DEL}:build:development\"/\"buildTarget\": \"${VAR_NAME_LOW}:build:development\"/i" "${NODE_FOLDER_PROJECT}/angular.json"
+  sed -i "s/\"buildTarget\": \"${VAR_NAME_DEL}:build\"/\"buildTarget\": \"${VAR_NAME_LOW}:build\"/i" "${NODE_FOLDER_PROJECT}/angular.json"
 fi
 
 exit 0
